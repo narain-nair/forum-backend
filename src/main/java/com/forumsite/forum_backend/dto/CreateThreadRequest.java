86c5fcc2f0df03cb5efd4b5511b95e16a2fd323a
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-10-22 20:02:20 +0100

    Basic thread request DTO

---
+++ src/main/java/com/forumsite/forum_backend/dto/CreateThreadRequest.java
@@ -0,0 +1,13 @@
+package com.forumsite.forum_backend.dto;
+
+public class CreateThreadRequest {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
