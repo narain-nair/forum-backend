43b5d5738c21f9354f3ec29a706a3563a1b71b5e
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-10-22 20:03:27 +0100

    PostDTO to wrap it all together, including a boolean switch to say whether it is deleted or not

---
+++ src/main/java/com/forumsite/forum_backend/dto/PostDTO.java
@@ -0,0 +1,42 @@
+package com.forumsite.forum_backend.dto;
+
+import java.time.LocalDateTime;
+
+public class PostDTO {
+    private Long id;
+    private String content;
+    private boolean deleted;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
