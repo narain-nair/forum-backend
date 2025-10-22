a4088824f4e622e4fec2762f55c0d235acd969c0
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-10-22 20:01:47 +0100

    Thread DTO created (will refer to other DTOs later)

---
+++ src/main/java/com/forumsite/forum_backend/dto/ThreadDTO.java
@@ -0,0 +1,43 @@
+package com.forumsite.forum_backend.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ThreadDTO {
+    private Long id;
+    private String title;
+    private LocalDateTime createdAt;
+    private List<PostDTO> posts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<PostDTO> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<PostDTO> posts) {
+        this.posts = posts;
+    }
+}
